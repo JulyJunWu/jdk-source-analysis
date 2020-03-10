e4f11d0b5206723ae46ac0afb369dc47eb9ec7d6
JulyJunWu <djian@linewell.com>
2020-03-11 00:15:41 +0800

    JDK源码

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Main {
+
+
+    public static void main(String [] args){
+        //测试
+        HashMap<Object, Object> hashMap = new HashMap<>();
+        hashMap.put("1",2);
+        System.out.println(hashMap.values().size());
+    }
+
+}
